f294c8b08980cd78dd6e409e1c9cde8bddc2c069
vagrant <vagrant@vagrant.vm>
2018-06-18 07:44:43 +0000

    Notes on CRUD with SQLAlchemy

--- SQLAlchemyNotes.py
+++ SQLAlchemyNotes.py
@@ -14,7 +14,7 @@ Base.metadata.bind = engine # bind our current database engine to the base class
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
-#### Notes: CREATE
+#### NOTE: CREATE
 myFirstRestaurant = Restaurant(name = "Pizza Palace") # Initialize an item representing a table
 session.add(myFirstRestaurant) # add the new table to the session in the engine
 session.commit()		# session.commit commits the changes in the actual database
@@ -27,11 +27,10 @@ session.add(cheesepizza)
 session.commit()
 
 
-## DELETING
-
-
+## NOTE: DELETE [early attempt for the sake of cleanliness and curiousity]
+##              [Real Version is Below]
 # Before I added this loop, I had added a bunch of restaurants named Pizza Palace
-# So I deleted them all. IMPORTANT TO NOTE that it's near impossible to delete the objects
+# So I deleted them all. IMPORTANT TO NOTICE that it's near impossible to delete the objects
 # with this item as a relationship once i've deleted this item and lost its handler.
 # THEREFORE, it's important to delete it's relationships first before deleting it I think.
 for item in restaurants:
@@ -48,5 +47,37 @@ for item in restaurants:
         session.delete(item) # deletes a table
 session.commit()
 
-## UPDATING
-session.commit() 
+## NOTE: UPDATE
+## Here's updating the price of all of the Veggie Burgers from all restaurants to $2.99
+                                        # Also Noticed he's using filter_by, which seems
+                                        # much more efficient
+veggieBurgers = session.query(MenuItem).filter_by(name = "Veggie Burger")
+for brgr in veggieBurgers:
+    if brgr.price != '$2.99':
+        brgr.price = '$2.99'
+        session.add(brgr)
+        session.commit()
+
+for brgr in veggieBurgers:
+    print(brgr.price)
+
+
+## NOTE: DELETE
+## (I already played with it before but this is the
+## playthrough from the official tutorial)
+
+try:
+    spinach = session.query(MenuItem).filter_by(name='Spinach Ice Cream').one()
+    print(spinach.restaurant.name, "has", spinach.name)
+    session.delete(spinach)
+    session.commit()
+except:
+    print("Spinach Ice Cream already deleted")
+
+
+
+try:
+    spinach = session.query(MenuItem).filter_by(name='Spinach Ice Cream').one()
+    print("Somewhere else has Spinach Ice Cream too!")
+except:
+    pass
