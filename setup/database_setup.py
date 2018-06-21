c762328fc8b8c3850faab2d45eaae987fa67f9fa
vagrant <vagrant@vagrant.vm>
2018-06-21 01:55:20 +0000

    Fixed randomly appearing empty db issue

--- setup/database_setup.py
+++ setup/database_setup.py
@@ -29,5 +29,13 @@ class MenuItem(Base):
 
 ############  insert at end of file #############
 
-engine = create_engine('sqlite:///restaurantmenu.db')
-Base.metadata.create_all(engine)
+
+# Textbook example of why if __name__ == '__main__':
+# is important. At first when i used this file to
+# create the DB, i forgot to put if __name__ == '__main__'
+# here, then, whenever I ran my code, even though I'd moved
+# my database elsewhere, it was initializing a new empty
+# restaurantmenu.db in whichever folder I was working in.
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///restaurantmenu.db')
+    Base.metadata.create_all(engine)
